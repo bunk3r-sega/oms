3474e462f5c5c7f4236469dd472536d39810d4d1
bunk3r-sega <n.uganov@yanex.ru>
2020-11-21 21:29:10 +0300

    routes & views

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/product', function () {
+    return view('product');
+})->middleware(['auth'])->name('product');
+
 
 require __DIR__.'/auth.php';
